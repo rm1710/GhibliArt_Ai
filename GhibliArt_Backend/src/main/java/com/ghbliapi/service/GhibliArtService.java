96a958941ba77e40239d1ecc882e71aaccfe8b62
rm1710 <d290mishraritesh@gmail.com>
2025-07-05 15:27:02 +0530

    new Ai Project

---
+++ GhibliArt_Backend/src/main/java/com/ghbliapi/service/GhibliArtService.java
@@ -0,0 +1,47 @@
+package com.ghbliapi.service;
+
+import com.ghbliapi.client.StabilityAIClient;
+import com.ghbliapi.dto.TextToImageRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class GhibliArtService {
+
+    private final StabilityAIClient stabilityAIClient;
+    private final String apiKey;
+
+    public GhibliArtService(StabilityAIClient stabilityAIClient, @Value("${stability.api.key}") String apiKey) {
+        this.stabilityAIClient = stabilityAIClient;
+        this.apiKey = apiKey;
+    }
+
+    public byte[] createGhibliArt(MultipartFile image, String prompt) {
+        String finalPrompt = prompt+", in the beautiful, detailed anime style of studio ghibli.";
+        String engineId = "stable-diffusion-v1-6";
+        String stylePreset = "anime";
+
+        return stabilityAIClient.generateImageFromImage(
+                "Bearer " + apiKey,
+                engineId,
+                image,
+                finalPrompt,
+                stylePreset
+        );
+    }
+
+    public byte[] createGhibliArtFromText(String prompt, String style) {
+        String finalPrompt = prompt+", in the beautiful, detailed anime style of studio ghibli.";
+        String engineId = "stable-diffusion-v1-6";
+        String stylePreset = style.equals("general") ? "anime" : style.replace("_", "-");
+
+        TextToImageRequest requestPayload = new TextToImageRequest(finalPrompt, stylePreset);
+
+        return stabilityAIClient.generateImageFromText(
+                "Bearer " + apiKey,
+                engineId,
+                requestPayload
+        );
+    }
+}
